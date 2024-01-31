2722290bf16e87e4ad1c246e1a18c6a71a242ec5
HumaCode <humaidizakaria1998@gmail.com>
2024-01-31 16:09:43 +0700

    creating tabIndex getx controlle & obx

--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -1,53 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:foody/constants/constants.dart';
+import 'package:foody/controllers/tab_index_controller.dart';
+import 'package:get/get.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: hieght,
-            width: width,
-            color: kOffWhite,
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Theme(
-              data: Theme.of(context).copyWith(canvasColor: kPrimary),
-              child: BottomNavigationBar(
-                showSelectedLabels: false,
-                showUnselectedLabels: false,
-                unselectedIconTheme: const IconThemeData(color: Colors.black38),
-                selectedIconTheme: const IconThemeData(color: kSecondary),
-                onTap: (value) {
-                  print(value);
-                },
-                items: const [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.search),
-                    label: 'Search',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.shopping_cart),
-                    label: 'Cart',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.person),
-                    label: 'Profile',
-                  ),
-                ],
-              ),
+    final controller = Get.put(TabIndexController());
+
+    return Obx(
+      () => Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              height: hieght,
+              width: width,
+              color: kOffWhite,
             ),
-          )
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Theme(
+                data: Theme.of(context).copyWith(canvasColor: kPrimary),
+                child: BottomNavigationBar(
+                  showSelectedLabels: false,
+                  showUnselectedLabels: false,
+                  unselectedIconTheme:
+                      const IconThemeData(color: Colors.black38),
+                  selectedIconTheme: const IconThemeData(color: kSecondary),
+                  onTap: (value) {
+                    controller.setTableIndex = value;
+                  },
+                  currentIndex: controller.tabIndex,
+                  items: const [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'Home',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.search),
+                      label: 'Search',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.shopping_cart),
+                      label: 'Cart',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.person),
+                      label: 'Profile',
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
