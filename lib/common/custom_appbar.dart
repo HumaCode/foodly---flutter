7ac8a3b96f6bc757d5c1c1f3a7362d26fa520b64
HumaCode <humaidizakaria1998@gmail.com>
2024-01-31 19:26:42 +0700

    custom appbar time function

--- lib/common/custom_appbar.dart
+++ lib/common/custom_appbar.dart
@@ -60,4 +60,17 @@ class CustomAppbar extends StatelessWidget {
       ),
     );
   }
+
+  String getTimeOfDay() {
+    DateTime now = DateTime.now();
+    int hour = now.hour();
+
+    if (hour >= 0 && hour < 12) {
+      return "";
+    } else if (hour >= 12 && hour < 16) {
+      return "";
+    } else {
+      return "";
+    }
+  }
 }
